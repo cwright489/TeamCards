f8a04ac245d2a1a9eb003e9b94fb4ef061f6e81d
Chance Wright <cwright489@gmail.com>
2020-03-29 13:22:04 -0600

    added intern js constructor

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,20 +1,18 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-const Intern = require("../lib/Intern");
+const Employee = require('./Employee');
 
-test("Can set school via constructor", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.school).toBe(testValue);
-});
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
 
-test("getRole() should return \"Intern\"", () => {
-  const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
-  expect(e.getRole()).toBe(testValue);
-});
+    getRole() {
+        return 'Intern'
+    }
+    getSchool() {
+        return this.school;
+    }
+}
 
-test("Can get school via getSchool()", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.getSchool()).toBe(testValue);
-});
+module.exports = Intern
