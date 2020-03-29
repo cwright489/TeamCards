32b7b214c55c1fbcd3a4c847abad9d403582b7ff
Chance Wright <cwright489@gmail.com>
2020-03-29 13:10:05 -0600

    added intern.js test

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Intern = require("../lib/Intern");
+
+test("Can set school via constructor", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
